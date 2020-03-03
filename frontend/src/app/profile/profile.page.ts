7be02d907f28340c6c44fbee6c8689ea21248a80
Zenahr Barzani <zenmatica@gmail.com>
2020-03-04 00:30:33 +0100

    Added Profile Login Page

--- frontend/src/app/profile/profile.page.ts
+++ frontend/src/app/profile/profile.page.ts
@@ -6,10 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.page.scss'],
 })
 export class ProfilePage implements OnInit {
+  model: any = {}
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onLogin(f) {
+    console.log(f.value)
   }
 
 }
