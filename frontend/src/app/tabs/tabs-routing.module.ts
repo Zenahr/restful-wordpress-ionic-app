29b0ca51ce861ed2f77e49a3df44f06e31cf93da
Zenahr Barzani <zenmatica@gmail.com>
2020-03-03 22:55:48 +0100

    added more routing

--- frontend/src/app/tabs/tabs-routing.module.ts
+++ frontend/src/app/tabs/tabs-routing.module.ts
@@ -18,22 +18,22 @@ const routes: Routes = [
         ]
       },
       {
-        path: 'tab2',
+        path: 'about',
         children: [
           {
             path: '',
             loadChildren: () =>
-              import('../tab2/tab2.module').then(m => m.Tab2PageModule)
+              import('../about/about.module').then(m => m.AboutPageModule)
           }
         ]
       },
       {
-        path: 'tab3',
+        path: 'profile',
         children: [
           {
             path: '',
             loadChildren: () =>
-              import('../tab3/tab3.module').then(m => m.Tab3PageModule)
+              import('../profile/profile.module').then(m => m.ProfilePageModule)
           }
         ]
       },
